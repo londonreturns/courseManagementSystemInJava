6d5725050433bb40700c462d8a6fb5e030a6daeb
londonreturns <subedioscar.47@gmail.com>
2024-01-22 15:41:41 +0545

    logo added

--- courseManagementSystem/src/courseManagementSystem/Main.java
+++ courseManagementSystem/src/courseManagementSystem/Main.java
@@ -3,5 +3,8 @@ package courseManagementSystem;
 public class Main {
 	public static void main(String[] args) {
 		StandardFrame loginFrame = new StandardFrame();
+		loginFrame.setTitle("Course Management System");
+		
+		
 	}	
 }
--- courseManagementSystem/src/courseManagementSystem/StandardFrame.java
+++ courseManagementSystem/src/courseManagementSystem/StandardFrame.java
@@ -1,5 +1,6 @@
 package courseManagementSystem;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
@@ -8,11 +9,15 @@ public class StandardFrame extends JFrame{
 	int width = 500;
 	int height = 500;
 	boolean visibility = true;
+	String logoPath = "assets/logo.png";
 	
 	StandardFrame(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(visibility);
 		setSize(width, height);
+		setResizable(false);
+		ImageIcon logo = new ImageIcon(logoPath);
+		setIconImage(logo.getImage());
 	}
 	
 	StandardFrame(int width, int height){
