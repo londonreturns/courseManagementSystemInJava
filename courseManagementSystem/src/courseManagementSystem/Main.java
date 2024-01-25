ce21623f8f1f8fbb991a2b40f90569613354c878
londonreturns <subedioscar.47@gmail.com>
2024-01-25 17:02:55 +0545

    package divided / font added

--- courseManagementSystem/src/courseManagementSystem/Main.java
+++ courseManagementSystem/src/courseManagementSystem/Main.java
@@ -1,33 +1,38 @@
 package courseManagementSystem;
 
-import java.awt.Color;
+import utility.LoginFrame;
+import utility.RegisterFrame;
 
 public class Main {
-	static LoginFrame loginFrame = new LoginFrame(200, 200, 1000, 750, Color.black);
-	static RegisterFrame registerFrame = new RegisterFrame(200, 200, 1000, 750, Color.GRAY);
+	static LoginFrame loginFrame = new LoginFrame(200, 200, 1000, 750);
+	static RegisterFrame registerFrame = new RegisterFrame(200, 200, 1000, 750);
 
 	public static void main(String[] args) {
-		loginFrameDiplay();
+		loginFrameDisplay();
+		
 	}
 
-	static void loginFrameDiplay() {
+	public static void loginFrameDisplay() {
 		if (loginFrame.loginInit == false) {
 			loginFrame.loginInit = true;
 			loginFrame.setLoginElements();
+			loginFrame.setLocation(registerFrame.getX(), registerFrame.getY());
 			loginFrame.setVisible(true);
 		}else {
+			loginFrame.setLocation(registerFrame.getX(), registerFrame.getY());
 			loginFrame.setVisible(true);
 		}
 		registerFrame.setVisible(false);
-		
 	}
 	
-	static void registerFrameDisplay() {
-		if (registerFrame.loginInit == false) {
-			registerFrame.loginInit = true;
+	public static void registerFrameDisplay() {
+		if (registerFrame.registerInit == false) {
+			registerFrame.registerInit = true;
 			registerFrame.setLoginElements();
+			registerFrame.setLocation(loginFrame.getX(), loginFrame.getY());
 			registerFrame.setVisible(true);
 		}else {
+			registerFrame.setLocation(loginFrame.getX(), loginFrame.getY());
 			registerFrame.setVisible(true);
 		}
 		loginFrame.setVisible(false);
