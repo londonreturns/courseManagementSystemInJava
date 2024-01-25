86a1688ed2ba44c8bb53c18af9c532f8d403c318
londonreturns <subedioscar.47@gmail.com>
2024-01-25 07:32:33 +0545

    logo added & login validation

--- courseManagementSystem/src/courseManagementSystem/StandardFrame.java
+++ courseManagementSystem/src/courseManagementSystem/StandardFrame.java
@@ -3,9 +3,9 @@ package courseManagementSystem;
 import java.awt.Color;
 import java.awt.Container;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
-@SuppressWarnings("serial")
 public class StandardFrame extends JFrame{
 	
 	int x_coord = 0;
@@ -18,12 +18,22 @@ public class StandardFrame extends JFrame{
 		setBounds(this.x_coord, this.y_coord, this.width, this.height);
 		setLayout(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
+		
+		ImageIcon logo = new ImageIcon("assets/logo.png");
+		setIconImage(logo.getImage());
+				
 	}
 	
 	StandardFrame(int x_coord, int y_coord, int width, int height){
 		setBounds(x_coord, y_coord, width, height);
 		setLayout(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
+
+		ImageIcon logo = new ImageIcon("assets/logo.png");
+		setIconImage(logo.getImage());
+			
 	}
 	
 	StandardFrame(int x_coord, int y_coord, int width, int height, Color color){
@@ -31,5 +41,10 @@ public class StandardFrame extends JFrame{
 		setLayout(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		container.setBackground(color);
+		setResizable(false);
+
+		ImageIcon logo = new ImageIcon("assets/logo.png");
+		setIconImage(logo.getImage());
+			
 	}
 }
