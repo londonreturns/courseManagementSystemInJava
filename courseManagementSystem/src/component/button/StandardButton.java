6214adae332ed88c0252736bf2a5c63cef35ca79
londonreturns <subedioscar.47@gmail.com>
2024-02-11 14:09:18 +0545

    StandardButton border

--- courseManagementSystem/src/component/button/StandardButton.java
+++ courseManagementSystem/src/component/button/StandardButton.java
@@ -2,17 +2,24 @@ package component.button;
 
 import java.awt.Cursor;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+
+import color.Color0;
 
 public class StandardButton extends JButton{
 	public StandardButton() {
 		Cursor handCursor = new Cursor(Cursor.HAND_CURSOR);
 		this.setCursor(handCursor);
+		this.setFocusable(false);
+		this.setBorder(BorderFactory.createLineBorder(new Color0()));
 	}
 	
 	StandardButton(String text){
 		super(text);
 		Cursor handCursor = new Cursor(Cursor.HAND_CURSOR);
 		this.setCursor(handCursor);
+		this.setFocusable(false);
+		this.setBorder(BorderFactory.createLineBorder(new Color0()));
 	}
 }
